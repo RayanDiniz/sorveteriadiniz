9803ca696d2b135c5016b362361729bafb492729
RayanDiniz <rayancassiokiol@gmail.com>
2020-04-18 01:37:20 -0300

    css

--- public/index.php
+++ public/index.php
@@ -30,6 +30,23 @@
         </div>
     </nav>
 
+    <div class="">
+        <img src="../img/logo001.png" class="img-fluid" alt="principal"/>
+    </div>
+
+    <div class="m-0 text-center bg-warning">
+        <div class="container mt-6">
+            <img src="../img/perfil.jpg" class="rounded-circle" alt="Rayan Diniz"/>
+        </div>
+        <div class="container rounded mt-3 m-auto bg-light">
+            <p>"Resolvir reabrir esse negocio de familia, pois para mim boas ideias nunca morrem"</p>
+        </div>
+    </div>
+
+    <div id="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d498.20797875489603!2d-44.26052769605909!3d-2.614915366133735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe5336dceed674711!2sSorveteria%20Diniz!5e0!3m2!1sen!2sbr!4v1576245018269!5m2!1sen!2sbr" title="localMapa"></iframe>
+    </div>
+
     <div class="footer">
         <footer id="myFooter">
             <div class="container">
