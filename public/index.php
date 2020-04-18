740be75fc95bba55451cee55ee89af8027e34348
RayanDiniz <rayancassiokiol@gmail.com>
2020-04-17 23:20:21 -0300

    up

---
+++ public/index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <div class="footer">
+        <footer id="myFooter">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3 text-center">
+                        <h2 class="logo"><a href="https://sorveteriadiniz.com"> <img src={logo} alt="Logo da Sorveteria Diniz"/> </a></h2>
+                        <ul>
+                            <li><em>Fone</em>: +55 98 3221-3386</li>
+                            <li><em>Email</em>: dinizsorveteria@gmail.com</li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-2">
+                        <h5>Local</h5>
+                        <ul>
+                            <li>Rua Sete de Setembro, 1, <br/>Vila Esperança</li>
+                            <li>São Luís, <br/>Maranhão</li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-2">
+                        <h5>Sobre-nós</h5>
+                        <ul>
+                            <li><a href="https://sorveteriadiniz.com">Informações da Empresa</a></li>
+                            <li><a href="https://sorveteriadiniz.com">Contato</a></li>
+                            <li><a href="https://sorveteriadiniz.com">Blog</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-2">
+                        <h5>Suporte</h5>
+                        <ul>
+                            <li><a href="https://sorveteriadiniz.com">FAQ</a></li>
+                            <li><a href="https://sorveteriadiniz.com">Chat</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-2 text-center">
+                    <h5>Siga-nos nas redes sociais</h5>
+                        <div class="social-networks">
+                            <SocialMediaIconsReact iconSize="5" url="https://twitter.com/SorveteriaDiniz" icon="twitter" backgroundColor="rgba(0,0,0,0)"/>
+                            <SocialMediaIconsReact iconSize="5" url="https://www.facebook.com/sorveteriadiniz/" icon="facebook" backgroundColor="rgba(0,0,0,0)"/>
+                            <SocialMediaIconsReact iconSize="5" url= "https://instagram.com/SorveteriaDiniz" icon="instagram" backgroundColor="rgba(0,0,0,0)"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <p>© 2020 Copyright - Sorveteria Diniz</p>
+            </div>
+        </footer>
+    </div>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
