5cb0a197c7139f0dece60511aec35360e93d9a7c
RayanDiniz <rayancassiokiol@gmail.com>
2020-04-18 00:27:53 -0300

    favicon

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@
     <title>Title</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <link rel="icon" href="favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
