689b828a426d82c6ef36b12fe40fcf3841b4efe0
RayanDiniz <rayancassiokiol@gmail.com>
2021-04-17 13:29:27 -0300

    docker

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
-import './global.css';
+import './App.css';
 
 import Home from './pages/Home';
 
-function App() {
+export default function App() {
   return (
-    <Home />
+    <div className="container">
+      <div className="row justify-content-center align-items-center text-center">
+        <div class="col-sm-5">
+          <img src={img_logo} className="logo" alt="principal" />
+          <div className="jumbotron">
+
+            <h1 className="display-4">Olá!</h1>
+            <p className="lead">Bem-Vindo a Sorveteria Diniz</p>
+            <hr className="my-2" />
+            <a href="./src/pages/Cardapio">Cardápio</a>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 };
-
-export default App;
